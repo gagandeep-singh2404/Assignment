5d921a09281d20f78e925f130fb4aff364090a61
gagandeep-singh2404 <37903834+gagandeep-singh2404@users.noreply.github.com>
2019-03-04 23:43:06 +0100

    commit changed^
    changes

---
+++ ppar/Assignment2/Assignment2/Commands/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Assignment2.Commands
+{
+    public class Command
+    {
+    
+        public Action SpecificAction { get; set; }
+        public string Desc { get; set; }
+
+        public void Execute() { SpecificAction(); }
+    }
+}
--- ppar/Assignment2/Assignment2/Main/Program.cs
+++ ppar/Assignment2/Assignment2/Main/Program.cs
@@ -1,51 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Assignment2.Commands;
+using Assignment2.Individuals;
 
 namespace Assignment2
 {
     class Program
     {
+        private static Dictionary<string, Command> commands;
+        private static Command initilizeCommand = new Command() { SpecificAction = InitialzeCommand, Desc = "Initize All important Command" };
+
         static void Main(string[] args)
         {
-            ICommand command;
-         
-
-            bool continueLoop = true;
-            string decision;
+            initilizeCommand.Execute();
+            string index;
 
             do
             {
-                Console.WriteLine("Welcome Mrs. Douchebag " +
-                "\r\nPress \"help\" to see the list of Commands");
+                foreach (KeyValuePair<string, Command> cd in commands) Console.WriteLine("{0}: {1}", cd.Key, cd.Value.Desc);
+                index = Console.ReadLine();
+                if (commands.ContainsKey(index)) commands[index].Execute();
+
+            } while (index != "0");
+        }
+
+        private static void InitialzeCommand()
+        {
+     
+            commands = new Dictionary<string, Command>()
+        {
+            ["0"] = new Command { SpecificAction= QuietApplication, Desc = "Exit the Applikation" },
+            ["1"] =  new Command { SpecificAction= AddUserHandler, Desc = "Add a new User" }, 
+            ["2"] =  new Command { SpecificAction = ListUser, Desc = "Show Overview of User" }, 
+            ["3"] =  new Command { SpecificAction= ShowUser, Desc = "Search User with ID" } 
+        };
+
+        }
+
+        private static void AddUserHandler()
+        {
+            int index;
+            Type type = typeof(Person);
+            Type[] types;
+            string decision;
+
+            // All specifiy types are chosen
+            types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes())
+                      .Where(p => type.IsAssignableFrom(p) && !p.IsAbstract).ToArray();
+
+
+            // get the right type of the user
+            while (true)
+            {
+                Console.WriteLine("What kind of User do you want to create: ");
+
+                for (int i = 0; i < types.Length; i++)
+                {
+                    Console.Write("[{0} {1}]", i.ToString(), types[i].Name);
+                }
+                Console.WriteLine("");
                 decision = Console.ReadLine();
 
-                switch (decision)
-                {
-                    case "help":
-                        command = new HelpCommand();
-                        break;
-                    case "0":
-                        command = new AddCommand();
-                        break;
-                    case "1":
-                        command = new ShowOverviewCommand();
-                        break;
-                    case "2":
-                        command = new SpecificSearchCommand();
-                        break;
-                    case "q":
-                        continueLoop = false;
-                        command = null;
-                        break;
-                    default:
-                        command = null;
-                        break;
-                }
+                if (!int.TryParse(decision, out index)) continue;
+                if (index < 0 || index > types.Length) continue;
 
-                if (command != null)  command.Execute();
-                
+                break;
+            }
 
-            } while (continueLoop);
+            // create Person and get his properties
+            Person person = (Person)Activator.CreateInstance(types[index]);
+            PropertyInfo[] properties = types[index].GetProperties().Where(x => Attribute.IsDefined(x, typeof(IndividualProperty))).ToArray();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                PropertyInfo propertyInfo = properties[i];
+                Console.Write("Please enter {0}: ", propertyInfo.Name);
+                var prop = propertyInfo.GetCustomAttribute<IndividualProperty>();
+                string value = Console.ReadLine();
+                bool isOk = true;
+
+                if (prop.Regex != null && !prop.Regex.IsMatch(value)) { isOk = false; i--; }
+                if (isOk) { propertyInfo.SetValue(person, value); }
+            }
         }
+        private static void QuietApplication() { Console.WriteLine("-----------Thank you for using our System----------"); }
+        private static void ListUser() { Console.WriteLine("ich bin gerade in ListUser"); }
+        private static void ShowUser() { Console.WriteLine("ich bin hier in Show User"); }
     }
 }
