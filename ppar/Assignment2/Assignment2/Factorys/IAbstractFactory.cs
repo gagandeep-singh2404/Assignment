5f7b6f481352a873b9642de9f7e337ababb11652
gagandeep-singh2404 <37903834+gagandeep-singh2404@users.noreply.github.com>
2019-03-06 19:35:08 +0100

    delete project because of problems

--- ppar/Assignment2/Assignment2/Factorys/IAbstractFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Assignment2.Factorys
-{
-    public interface IAbstractFactory
-    {
-        IAbstractFactory CreateDBFactory();
-        IDBMapper CreateDBMapper();
-    }
-}
