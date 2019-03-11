f53016ef31f406649ac99b38eda572f6ed144c6f
gagandeep-singh2404 <37903834+gagandeep-singh2404@users.noreply.github.com>
2019-03-11 14:25:08 +0100

    deleted the odl version

--- cLessons/trunk/basic_topics/task11/task11.c
+++ cLessons/trunk/basic_topics/task11/task11.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
-#define NULL ((void *) 0)
-
-
+#define null 0
+#include <string.h> 
 char* mygetenv(const char *name);
 int mysetenv(const char *name, const char *value, int overwrite);
-
+extern char **environ; 
 int main(int argc, char* argv[], char*env[]){
     
-    mygetenv(*env);
-   
-    return 0;
+    if (argc < 2) {
+        printf("Please enter a valid input\n");
+        return 0;
+    }
 
+     char* nu =  mygetenv(*env);
+
+        if (nu == NULL) {
+          printf("nul");
+          for(char* e = nu; e != 0; ++e) {
+          printf("%s",e);
+        }
+      }
+    
+    return 0;
+}
 
 char* mygetenv(const char *name){
-    
-    if (name[0] == NULL) {
-        printf("(null)");
-        return NULL;
+    for(char** i = environ; *i != 0; ++i)
+    {
+      char* found = strstr(*i, name);
+      if (found != *i) continue;
+      char copy [strlen(*i)];
+      strcpy(copy, name);
+      if (strcmp(name,strtok(copy,"=")) == 0) {
+        printf("not here");
+        return *i;
+      }  
     }
-    printf("every thinh is ok");
-    return "ok";
+    printf("jo vergess es");
+return NULL;
 }
 
 int mysetenv(const char *name, const char *value, int overwrite){
--- ppar/Assignment1/Assignment1/Engine.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace Assignment1
-{
-    public interface IEngine
-    {
-       
-    }
-}
