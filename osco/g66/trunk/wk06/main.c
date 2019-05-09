0803eb930d821e5077944f6594864c6f397eddd4
Gagandeep Singh <37903834+gagandeep-singh2404@users.noreply.github.com>
2019-05-09 12:33:27 +0100

    this stuff

--- osco/g66/trunk/wk06/main.c
+++ osco/g66/trunk/wk06/main.c
@@ -1,13 +1,36 @@
 #include "ntk.h"
-#include "ntk.c"
 
+unsigned __stdcall tascCode(void* args);
+void part0();
 
-main() {
-int par;
-startNTK();
-task* t1=(task*)malloc(sizeof(task));
-create_task(t1,printf("hello world"),&par,sizeof(int),0);
-printTaskCB(t1);
-printTIB();
-delete_task(t1);
-free(t1);
+int main(void) {
+	int par = 5;
+	startNTK();
+	task* t1 = (task*)malloc(sizeof(task));
+	create_task(t1, tascCode, &par, sizeof(int), 0);
+	Sleep(1000);
+	part0();
+	
+	return 0;
+
+}
+
+unsigned __stdcall tascCode(void* args) {
+	task* t1 = (task*)args;
+	int* a = (int*)getArgument_task(t1);
+	Sleep(a[0]*1000);
+	
+	return 0;
+
+}
+
+void part0() {
+	int par;
+	for (int i = 5; i > 0; i--)
+	{
+		par = i;
+		task* t1 = (task*)malloc(sizeof(task));
+		create_task(t1, tascCode, &par, sizeof(int), 0);
+	}
+	
+}
